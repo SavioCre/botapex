e7cf1b1e7d5c0f6d344180840ef98c3e7c29da97
SavioCre <82991494+SavioCre@users.noreply.github.com>
2021-04-22 00:28:12 -0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,38 @@
+discordBotToken = "ODM0NTYzMDU1ODA2ODQwODUy.YICtaQ.agk3o41zy5AUHxHecN0AFPsuKTQ"
+
+config = {
+  "apiKey": "AIzaSyA8CiksDVO7fpJkDl6e_Ap1r7RnN3j26fU",
+  "authDomain": "bot-apex-discord-c3b55.firebaseapp.com",
+  "databaseURL": "https://bot-apex-discord-c3b55-default-rtdb.firebaseio.com/",
+  "storageBucket": "bot-apex-discord-c3b55.appspot.com"
+}
+
+trnApiKey= ["34497f25-72e3-47ae-afb0-ca47c3c17de5"]
+getLevel = "https://public-api.tracker.gg/apex/v1/standard/profile/"
+channelName = "stats" #YOU CAN DELETE THE IF STATEMENT TO MAKE IT WORK ON ALL CHANNELS
+reactionChannelNameBrazil = "bem-vindo" #REPLACE IT OR WHATEVER YOU WANNA DO, YOU DON'T NEED TO CARE ABOUT IT IF YOU ARE NOT IN BRAZIL
+reactionChannelNameEverywhere = "welcome" #REPLACE IT FOR THE DESIRED NAME FOR THE REACTION CHANNEL
+
+ranks = [(0,"Bronze IV","Bronze IV"),
+        (300,"Bronze III","Bronze III"),
+        (600,"Bronze II","Bronze II"),
+        (900,"Bronze I","Bronze I"),
+        (1200,"Prata IV", "Silver IV"),
+        (1600,"Prata III", "Silver III"),
+        (2000,"Prata II", "Silver II"),
+        (2400,"Prata I", "Silver I"),
+        (2800,"Ouro IV", "Gold IV"),
+        (3300,"Ouro III", "Gold III"),
+        (3800,"Ouro II", "Gold II"),
+        (4300,"Ouro I", "Gold I"),
+        (4800,"Platina IV", "Platinum IV"),
+        (4800,"Platina IV", "Platinum IV"),
+        (5400,"Platina III", "Platinum III"),
+        (6000,"Platina II", "Platinum II"),
+        (6600,"Platina I", "Platinum I"),
+        (7200,"Diamante IV", "Diamond IV"),
+        (7900,"Diamante III", "Diamond III"),
+        (8600,"Diamante II", "Diamond II"),
+        (9300,"Diamante I", "Diamond I"),
+        (10000,"Master", "Mestre"),
+        (-1, "Predator", "Predador")]
